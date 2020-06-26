c17a594b023b77e51288967e45d3d963853bc1e0
pjshin3 <tlsvlfwp1@naver.com>
2020-06-27 07:28:26 +0900

    #1 compose 텍스트 뷰 화면구성

--- app/src/main/java/com/example/jetpack_compose/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_compose/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.jetpack_compose
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.compose.Composable
+import androidx.ui.core.Modifier
+import androidx.ui.core.setContent
+import androidx.ui.foundation.Text
+import androidx.ui.layout.Column
+import androidx.ui.layout.padding
+import androidx.ui.tooling.preview.Preview
+import androidx.ui.unit.dp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        setContent {
+            Text("HONG")
+            helloWorldText()
+        }
+    }
+
+    @Preview
+    @Composable
+    fun helloWorldText(){
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text("Hello World")
+            Text("Hello World")
+            Text("Hello World")
+        }
     }
 }
